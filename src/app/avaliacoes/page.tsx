600915234edd753dc2f89b9ec10b7891d6c9751d
Nicolas Tomasi da Silva <tomasitrabalho@gmail.com>
2025-07-13 16:13:40 -0300

    first commit

---
+++ src/app/avaliacoes/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+
+export default function Avaliacoes() {
+  return (
+    <>
+      <ModeToggle />
+      <Button>BOTAO</Button>
+    </>
+  );
+}
