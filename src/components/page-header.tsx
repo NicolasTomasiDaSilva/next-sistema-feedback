3b3eba5e0de2f389ac4fe0100941a1f9f58b1382
Nicolas Tomasi da Silva <tomasitrabalho@gmail.com>
2025-07-13 22:49:13 -0300

    .

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -9,10 +9,10 @@ export function PageHeader() {
   const pageTitle = usePageTitle();
 
   return (
-    <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+    <header className="bg-background sticky top-0 flex h-20 shrink-0 items-center gap-2 border-b px-4">
       <SidebarTrigger className="-ml-1" />
       <Separator orientation="vertical" className="mr-2 h-4" />
-      <span>{pageTitle}</span>
+      <span className="text-xl font-bold">{pageTitle}</span>
       <div className="ml-auto">
         <ThemeToggle />
       </div>
