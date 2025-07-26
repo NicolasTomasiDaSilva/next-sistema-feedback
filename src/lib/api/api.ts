d351a58d228a996b66f40a577f74510e9afdf25d
Nicolas Tomasi da Silva <tomasitrabalho@gmail.com>
2025-07-26 14:23:18 -0300

    .

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,11 +1,35 @@
+import { User } from "@/schemas/user-schema";
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http:/localhost:3000/api",
+  baseURL: "http://localhost:3000/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export default api;
+async function request<TRequest, TResponse>({
+  url,
+  method,
+  data,
+}: {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  url: string;
+  data: TRequest;
+}): Promise<TResponse> {
+  return await api.request({
+    url: url,
+    method: method,
+    data: data,
+  });
+}
+
+function post<TRequest, TResponse>(
+  url: string,
+  data: TRequest
+): Promise<TResponse> {
+  return request<TRequest, TResponse>({ url: url, method: "POST", data: data });
+}
+
+const a: User = await post("/users", {});
