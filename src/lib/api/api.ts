be4053782c5a544e5ed5b6ae64d756d0f53ab2ff
Nicolas Tomasi da Silva <tomasitrabalho@gmail.com>
2025-07-26 14:52:04 -0300

    .

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,7 +1,14 @@
-import { User } from "@/schemas/user-schema";
 import axios, { AxiosInstance } from "axios";
 
-const api: AxiosInstance = axios.create({
+export const api: {
+  get: typeof get;
+  post: typeof post;
+} = {
+  get: get,
+  post: post,
+};
+
+const axiosInstance: AxiosInstance = axios.create({
   baseURL: "http://localhost:3000/api",
   timeout: 10000,
   headers: {
@@ -12,24 +19,61 @@ const api: AxiosInstance = axios.create({
 async function request<TRequest, TResponse>({
   url,
   method,
+  headers,
   data,
+  params,
 }: {
   method: "GET" | "POST" | "PUT" | "DELETE";
   url: string;
-  data: TRequest;
+  headers?: Record<string, string>;
+  params?: Record<string, any>;
+  data?: TRequest;
 }): Promise<TResponse> {
-  return await api.request({
+  const defaultHeaders = axiosInstance.defaults.headers.common || {};
+  const mergedHeaders = {
+    ...defaultHeaders,
+    ...headers,
+  };
+
+  return await axiosInstance.request({
     url: url,
     method: method,
     data: data,
+    headers: mergedHeaders,
+    params: params,
   });
 }
 
-function post<TRequest, TResponse>(
-  url: string,
-  data: TRequest
-): Promise<TResponse> {
-  return request<TRequest, TResponse>({ url: url, method: "POST", data: data });
+async function get<TResponse>({
+  url,
+  headers,
+  params,
+}: {
+  url: string;
+  headers?: Record<string, string>;
+  params?: Record<string, any>;
+}): Promise<TResponse> {
+  return await request<undefined, TResponse>({
+    url: url,
+    method: "GET",
+    headers: headers,
+    params: params,
+  });
 }
 
-const a: User = await post("/users", {});
+async function post<TRequest, TResponse>({
+  url,
+  headers,
+  data,
+}: {
+  url: string;
+  headers?: Record<string, string>;
+  data: TRequest;
+}): Promise<TResponse> {
+  return await request<TRequest, TResponse>({
+    url: url,
+    method: "POST",
+    headers: headers,
+    data: data,
+  });
+}
